f33de09bf12accd0324ad7643f9fb6b21653d36f
Haoran Yu <haoranyu6@gmail.com>
2020-03-26 04:10:39 -0700

    batch the index requests

--- kafka-consumer-elasticsearch/src/main/java/com/github/hyu6/kafka/consumer/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/github/hyu6/kafka/consumer/ElasticSearchConsumer.java
@@ -12,8 +12,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -64,7 +65,7 @@ public class ElasticSearchConsumer {
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // latest, earliest, none
         properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // disable auto commit of offsets
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
 
         // create the consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
@@ -91,7 +92,11 @@ public class ElasticSearchConsumer {
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
-            logger.info("Received " + records.count() + " records");
+            Integer recordCount = records.count();
+            logger.info("Received " + recordCount + " records");
+
+            BulkRequest bulkRequest = new BulkRequest();
+
             for (ConsumerRecord<String, String> record : records) {
 
                 // 2 strategies
@@ -99,28 +104,31 @@ public class ElasticSearchConsumer {
                 // String id = record.topic() + "_" + record.partition() + "_" + record.offset();
 
                 // twitter feed specific id
-                String id = extractIdFromTweet(record.value());
-
-                // insert data into ElasticSearch
-                IndexRequest indexRequest = new IndexRequest("twitter")
-                        .id(id) // make the consumer idempotent
-                        .source(record.value(), XContentType.JSON);
-
-                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-                logger.info(indexResponse.getId());
                 try {
-                    Thread.sleep(10); // introduce a small delay
+                    String id = extractIdFromTweet(record.value());
+
+
+                    // insert data into ElasticSearch
+                    IndexRequest indexRequest = new IndexRequest("twitter")
+                            .id(id) // make the consumer idempotent
+                            .source(record.value(), XContentType.JSON);
+
+                    bulkRequest.add(indexRequest); // add to bulk request (takes no time)
+                } catch (NullPointerException e) {
+                    logger.info("Skipping bad data: " + record.value());
+                }
+            }
+
+            if (recordCount > 0) {
+                BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                logger.info("Committing offsets...");
+                consumer.commitSync();
+                logger.info("Offsets committed");
+                try {
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }
-            logger.info("Committing offsets...");
-            consumer.commitSync();
-            logger.info("Offsets committed");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }
 
